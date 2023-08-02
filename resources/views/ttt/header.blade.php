c6bb22602d3f6a605fa284e26a3bdf2c63ce9ea5
Sergey <1@php-cat.com>
2023-08-02 14:44:23 +0500

    1212

--- resources/views/ttt/header.blade.php
+++ resources/views/ttt/header.blade.php
@@ -65,7 +65,7 @@
             height: 350px;
           ">
         <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
-            style="background-color: rgba(0, 0, 0, 0.75)">
+            style="background-color: rgba(0, 0, 0, 0.15)">
             <div class="flex h-full items-center justify-center">
                 <div class="px-6 text-center text-white md:px-12">
                     <h1 class="mb-6 text-5xl font-bold">Обучение бильярду в Тюмени</h1>
