d031cbbad12978e2cc2fdbffbe14e8ecd333686d
Sergey <1@php-cat.com>
2023-08-07 06:01:01 +0500

    атрактор обновил

--- resources/views/atraktor/layouts/header.blade.php
+++ resources/views/atraktor/layouts/header.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-      <a class="navbar-brand" href="/">Склад Документов</a>
+      <a class="navbar-brand" href="/">атрактор.рф</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
           aria-label="Переключатель навигации">
